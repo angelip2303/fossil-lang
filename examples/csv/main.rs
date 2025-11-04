54e262175f77da54a7c795ce088c1a35e5ff4b63
Ángel Iglesias Préstamo <angel.iglesias.prestamo@gmail.com>
2025-11-04 23:02:14 +0100

    fix: improving the examples

--- examples/csv/main.rs
+++ examples/csv/main.rs
@@ -1,27 +1,11 @@
 fn main() -> Result<(), ikigai_core::error::CompileError> {
     let source = r#"
-        open Data.Csv as Csv
-        open Random as Random
-
-        type Person = Csv<"examples/csv/people.csv">
+        type Person = Data.Csv<"examples/csv/people.csv">
         let people = Person.load "examples/csv/people.csv"
-
-        type PersonWithAge = {
-            name: string,
-            age: int
-        }
-
-        let peopleWithAge =
-            people
-                |> map (fun person -> {
-                    name = person.name,
-                    age = Random.int 0 100
-                } :> PersonWithAge)
-
-        Csv.write peopleWithAge "examples/csv/people_with_age.csv"
     "#;
 
-    ikigai_core::compile(source)?;
+    // Compile y ejecutar
+    ikigai_core::compile_and_run(source)?;
 
     Ok(())
 }
